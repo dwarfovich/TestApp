58e95e5f25454ee7dc4402d10df6c25e5f0e5e54
Tim Gilevich <ouncetim@gmail.com>
2023-10-26 21:47:03 +0300

    WIP

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "nlohmann/json.hpp"
+#include "TestLib/TestFunctions.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    nlohmann::json json;
+    json["abc"] = 765;
+    std::cout << "Hello, World!\n";
+    std::cout << json << std::endl;
+    tl::TestFunction();
+    tl::TestFunction2();
 }
